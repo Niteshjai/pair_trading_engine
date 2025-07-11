a122e96bbf10b38dd88dd1b37efd6f4d2bc7d95e
Niteshjai <147681425+Niteshjai@users.noreply.github.com>
2025-07-11 13:00:13 +0545

    Add files via upload

---
+++ data.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+
+sp100_tickers = [
+    'AAPL', 'ABBV', 'ABT', 'ACN', 'ADBE', 'AIG', 'AMD', 'AMGN', 'AMT', 'AMZN',
+    'AVGO', 'AXP', 'BA', 'BAC', 'BIIB', 'BK', 'BKNG', 'BLK', 'BMY', 'BRK-B',
+    'C', 'CAT', 'CHTR', 'CL', 'CMCSA', 'COF', 'COP', 'COST', 'CRM', 'CSCO',
+    'CVS', 'CVX', 'DHR', 'DIS', 'DOW', 'DUK', 'EMR', 'EXC', 'F', 'FDX',
+    'GD', 'GE', 'GILD', 'GM', 'GOOG', 'GOOGL', 'GS', 'HD', 'HON', 'IBM',
+    'INTC', 'INTU', 'ISRG', 'JNJ', 'JPM', 'KHC', 'KMI', 'KO', 'LIN', 'LLY',
+    'LMT', 'LOW', 'MA', 'MCD', 'MDLZ', 'MDT', 'MET', 'META', 'MMM', 'MO',
+    'MRK', 'MS', 'MSFT', 'NEE', 'NFLX', 'NKE', 'NVDA', 'ORCL', 'PEP', 'PFE',
+    'PG', 'PM', 'PYPL', 'QCOM', 'RTX', 'SBUX', 'SCHW', 'SO', 'SPG', 'T',
+    'TGT', 'TMO', 'TMUS', 'TSLA', 'TXN', 'UNH', 'UNP', 'UPS', 'USB', 'V',
+    'VZ', 'WBA', 'WFC', 'WMT', 'XOM'
+]
+
+def collect_data():
+    period='5Y'
+    stock_data = yf.download(sp100_tickers, period=period, interval='1d')['Close']
+    log_prices=np.log(stock_data)
+    log_prices.to_csv('log_prices.csv')
+    
